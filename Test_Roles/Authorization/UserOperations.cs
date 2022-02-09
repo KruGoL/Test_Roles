dd43c0a6c1ecd73b30d01d312a6ab9976a227ff8
Alexander Khrushkov <alexander.khrushkov@ttu.ee>
2022-02-09 17:39:35 +0200

    Add project files.

---
+++ Test_Roles/Authorization/UserOperations.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization.Infrastructure;
+
+namespace Test_Roles.Authorization
+
+{
+    public static class UserOperations
+    {
+        
+    }
+
+    public class UserRoles
+    {
+        
+        public static readonly string AdministratorRole = "Admin";
+        public static readonly string TrainerRole = "Trainer";
+        public static readonly string AthleteRole = "Athlete";
+
+    }
+}
---
+++ Test_Roles/Data/SeedData.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using Test_Roles.Authorization;
+
+namespace Test_Roles.Data
+{
+    public class SeedData
+    {
+        public static async Task Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ApplicationDbContext(
+                       serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+            {
+                // For sample purposes seed both with the same password.
+                // Password is set with the following:
+                // dotnet user-secrets set SeedUserPW <pw>
+                // The admin user can do anything
+
+
+                // allowed user can create and edit contacts that they create
+                
+                await EnsureRole(serviceProvider, UserRoles.AdministratorRole);
+                await EnsureRole(serviceProvider, UserRoles.TrainerRole);
+                await EnsureRole(serviceProvider, UserRoles.AthleteRole);
+
+            }
+        }
+
+        private static async Task EnsureRole(IServiceProvider serviceProvider, string role)
+        {
+            var roleManager = serviceProvider.GetService<RoleManager<IdentityRole>>();
+
+            if (roleManager == null)
+            {
+                throw new Exception("roleManager null");
+            }
+
+            IdentityResult IR;
+            if (!await roleManager.RoleExistsAsync(role))
+            {
+                IR = await roleManager.CreateAsync(new IdentityRole(role));
+            }
+
+        }
+        
+    }
+}
